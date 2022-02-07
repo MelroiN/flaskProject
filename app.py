9569e65b147578e4170c288d04e191afd0eaf4c1
MelroiN <exregius@gmail.com>
2022-02-08 01:28:04 +0200

    Added templates in .html pages

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ['Установка', 'Первое приложение', 'Обратная связь']
+
 
 @app.route('/')
-def index():  # put application's code here
-    return render_template('index.html')
+def index():
+    return render_template('index.html', title="Про Flask", menu=menu)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="О сайте", menu=menu)
 
 
 if __name__ == '__main__':
